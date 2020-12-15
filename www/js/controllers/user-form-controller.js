3d1352bc5ace76656e6cd9dc9c9663d1aec2c5d2
Manoj <>
2020-12-15 18:59:13 -0500

    Show Users Page on click of button

--- www/js/controllers/user-form-controller.js
+++ www/js/controllers/user-form-controller.js
@@ -1,9 +1,13 @@
 angular.module('ses.user-form-controller', [])
 
-.controller('UserFromCtrl', function($scope) {
+.controller('UserFromCtrl', function($scope, $state) {
   $scope.userData = {};
 
   $scope.saveUser = () => {
     console.log('Saving User Data' , $scope.userData);
+  }
+
+  $scope.showUsers = () => {
+    $state.go('users');
   }
 })
