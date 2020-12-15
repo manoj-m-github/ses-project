27810c836f4ffb375705be6129a7b786377b49e8
Manoj <>
2020-12-15 14:22:15 -0500

    Add user modal data and save the form

--- www/js/controllers/user-form-controller.js
+++ www/js/controllers/user-form-controller.js
@@ -1,5 +1,9 @@
 angular.module('ses.user-form-controller', [])
 
 .controller('UserFromCtrl', function($scope) {
-    
+  $scope.userData = {};
+
+  $scope.saveUser = () => {
+    console.log('Saving User Data' , $scope.userData);
+  }
 })
