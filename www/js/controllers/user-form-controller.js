545a9a6a1959b652efee52dc9717cca183474ba2
Manoj <>
2020-12-15 19:41:56 -0500

    Show an Alert and Clear Form Data after adding an user

--- www/js/controllers/user-form-controller.js
+++ www/js/controllers/user-form-controller.js
@@ -1,11 +1,38 @@
 angular.module('ses.user-form-controller', [])
 
-.controller('UserFromCtrl', function($scope, $state) {
+.controller('UserFromCtrl', function($scope, $state, $ionicPopup, Users) {
   $scope.userData = {};
 
   $scope.saveUser = () => {
-    console.log('Saving User Data' , $scope.userData);
+    if (!$scope.userData) {
+      $ionicPopup.alert({
+        title: 'Alert',
+        template: 'Please fill all details'
+      });
+      return;
+    }
+    let user = {
+      firstName: $scope.userData.firstName,
+      lastName: $scope.userData.lastName,
+      gender: $scope.userData.gender,
+      dateOfBirth: new Date($scope.userData.dateOfBirth).toDateString(),
+    };
+    // Add User
+    Users.addUser(user);
+
+    // Show Alert after adding User
+    var alertPopup = $ionicPopup.alert({
+      title: 'Success',
+      template: 'Save an User'
+    });
+
+    // Clear User Form after adding an user
+    alertPopup.then(() => {
+      $scope.userData = {}
+    });
   }
+
+  $scope.isFormValid
 
   $scope.showUsers = () => {
     $state.go('users');
